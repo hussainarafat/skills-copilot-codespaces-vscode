16a55b1adffb9e8ede6cdc84ff5a2f71fbfa5e21
hussainarafat <134585584+hussainarafat@users.noreply.github.com>
2023-07-20 22:45:17 +0000

    Copilot 3rd Commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// Create web server
+
+// Import modules
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// Import models
+const Comment = require('../models/Comment');
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+// Import middleware
+const auth = require('../middleware/auth');
+ //Handle request to get all comments
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}
+);
